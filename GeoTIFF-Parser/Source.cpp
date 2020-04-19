3f2ec49820a66516c78ee5856d95bd2d926a7dfb
Shihabyooo <shihabnoor@hotmail.com>
2020-04-19 11:23:18 +0200

    Began implementing Deflate decompression.

--- GeoTIFF-Parser/Source.cpp
+++ GeoTIFF-Parser/Source.cpp
@@ -647,6 +647,67 @@ void ParseUncompressedStripOrTileData(int stripOrTileID)
 
 }
 
+void ParseDeflateStripOrTileData(int stripOrTileID)
+{
+
+	std::cout << "Attempting to decompress a Deflate data" << std::endl;
+	stream.seekg(tiffDetails.tileStripOffset.get()[stripOrTileID]);
+
+	short int compressionMethod, compressionInfo;
+	short int checkBits, presetDictionary, compressionLevel;
+	int windowSize;
+	char byte[1];
+
+	//Check first byte
+	stream.read(byte, sizeof(byte));
+	
+	//divide byte into two 4-bits and check them
+	compressionMethod = ((unsigned char)byte[0] & 0x0F); //filter out the upper 4 bits, leave the lower 4.
+	compressionInfo = ((unsigned char)byte[0] & 0xF0) >> 4; //filter out the lower 4 bits and shift the upper 4 down.
+	
+	if (compressionMethod != 8)
+		std::cout << "WARNING! The Deflate header's CINFO field is not equal to 8. At strip/tile: " << stripOrTileID << std::endl;
+
+	windowSize = pow(2, (compressionInfo + 8));
+
+	//Check the second byte
+	stream.read(byte, sizeof(byte));
+
+	checkBits = ((unsigned char)byte[0] & 0x1F); //filter out the upper 3 bits, leave lower 5.
+	presetDictionary = ((unsigned char)byte[0] & 0x20) >> 5; //filter out everything but the 6th bit and shift it down.
+	compressionLevel = ((unsigned char)byte[0] & 0x30) >> 6; //filter out everything but the last two bits and shift them down.
+
+	if (presetDictionary != 0)
+		std::cout << "WARNING! The Deflate FDICT field is set. This is against the TIFF6.0 standard. At strip/tile: " << stripOrTileID << std::endl;
+
+
+	//check first block header (3 bits)
+	short int blockHeaderBit, blockType[2];
+	
+	stream.read(byte, sizeof(byte));
+	
+	//test
+	short int bits[8];
+	bits[0] = ((unsigned char)byte[0] & 0x01);
+	bits[1] = ((unsigned char)byte[0] & 0x02) >> 1;
+	bits[2] = ((unsigned char)byte[0] & 0x04) >> 2;
+	bits[3] = ((unsigned char)byte[0] & 0x08) >> 3;
+	bits[4] = ((unsigned char)byte[0] & 0x10) >> 4;
+	bits[5] = ((unsigned char)byte[0] & 0x20) >> 5;
+	bits[6] = ((unsigned char)byte[0] & 0x40) >> 6;
+	bits[7] = ((unsigned char)byte[0] & 0x80) >> 7;
+	std::cout << "bits: " << bits[7] << bits[6] << bits[5] << bits[4] << bits[3] << bits[2] << bits[1] << bits[0] << std::endl;
+	//endtest
+
+
+
+	//test output
+	std::cout << "Compression Method: " << compressionMethod << ", Compression Info: " << compressionInfo << std::endl;
+	std::cout << "Window Size: " << windowSize << std::endl;	
+	std::cout << "Check Bits: " << checkBits << ", Preset Dictionary: " << presetDictionary << ", Compression Level: " << compressionLevel << std::endl;
+
+}
+
 bool ParseStripOrTileData(int stripOrTileID)
 {
 	switch (tiffDetails.compression)
@@ -691,8 +752,9 @@ bool ParseStripOrTileData(int stripOrTileID)
 		break;
 
 	case (8): //Deflate
-		std::cout << "ERROR! Unsupported compression algorithm" << std::endl;
-		return false;
+		/*std::cout << "ERROR! Unsupported compression algorithm" << std::endl;
+		return false;*/
+		ParseDeflateStripOrTileData(stripOrTileID);
 		break;
 
 	case (9): //"Defined by TIFF-F and TIFF-FX standard (RFC 2301) as ITU-T Rec. T.82 coding, using ITU-T Rec. T.85 (which boils down to JBIG on black and white). "
@@ -717,9 +779,9 @@ bool ParseStripOrTileData(int stripOrTileID)
 
 bool main()
 {
-	//stream.open("dem_GCS.tif", std::ios::binary | std::ios::in);
+	stream.open("dem_GCS.tif", std::ios::binary | std::ios::in);
 	//stream.open("testTIFF.tif", std::ios::binary | std::ios::in);
-	stream.open("testTIFF_32.tif", std::ios::binary | std::ios::in);
+	//stream.open("testTIFF_32.tif", std::ios::binary | std::ios::in);
 
 	if (!stream.is_open())
 	{
