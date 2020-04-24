ad5637c4015d72e15dfece03c37a0e7fa85894dd
Shihabyooo <shihabnoor@hotmail.com>
2020-04-24 09:52:39 +0200

    Added float32-based test rasters, moved test data to subfolder

--- GeoTIFF-Parser/Source.cpp
+++ GeoTIFF-Parser/Source.cpp
@@ -3,17 +3,14 @@
 
 int main()
 {
-	
-	//LoadGeoTIFF("dem_GCS.tif");
-	//LoadGeoTIFF("dem_GSC_packbits.tif");
-	//LoadGeoTIFF("alos_UTM_nocomp_int16.tif");
-	LoadGeoTIFF("alos_UTM_packbits_int16.tif");
-	//LoadGeoTIFF("dem_GSC_uncomp.tif");
-	//LoadGeoTIFF("dem_UTM_uncomp.tif");
-	//LoadGeoTIFF("testTIFF.tif");
-	//LoadGeoTIFF("testTIFF_32.tif");
+	//LoadGeoTIFF("TestData\\alos_UTM_nocomp_int16.tif");
+	//LoadGeoTIFF("TestData\\alos_UTM_packbits_int16.tif");
+	//LoadGeoTIFF("TestData\\alos_UTM_packbits_float64.tif");
+	LoadGeoTIFF("TestData\\alos_UTM_packbits_float32.tif");
+	//LoadGeoTIFF("TestData\\testTIFF.tif");
+	//LoadGeoTIFF("TestData\\testTIFF_32.tif");
 	
 	std::cout << "FINISHED!" << std::endl; //test
-	
+	UnloadGeoTIFF();
 	return 0;
 }
