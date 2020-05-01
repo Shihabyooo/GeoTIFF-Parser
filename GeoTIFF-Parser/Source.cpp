308c16e58dc654560f2c06cda90ee7be6ddf9ef0
Shihabyooo <shihabnoor@hotmail.com>
2020-05-01 15:00:17 +0200

    Added raster extent calculation, getter of a pointer to BitMap for use externally.

--- GeoTIFF-Parser/Source.cpp
+++ GeoTIFF-Parser/Source.cpp
@@ -8,13 +8,13 @@ int main()
 	//LoadGeoTIFF("TestData\\alos_UTM_packbits_float64.tif");
 	//LoadGeoTIFF("TestData\\alos_UTM_packbits_float32.tif");
 	//LoadGeoTIFF("TestData\\alos_UTM_packbits_int32.tif");
-	//LoadGeoTIFF("TestData\\alos_UTM_nocomp_float32.tif");
-	LoadGeoTIFF("TestData\\alos_UTM_deflate_uint16.tif");
+	LoadGeoTIFF("TestData\\alos_UTM_nocomp_float32.tif");
+	//LoadGeoTIFF("TestData\\alos_UTM_deflate_uint16.tif");
 	//LoadGeoTIFF("TestData\\alos_UTM_deflate_float32.tif");
 	//LoadGeoTIFF("TestData\\testTIFF.tif");
 	//LoadGeoTIFF("TestData\\testTIFF_32.tif");
 	
-
+	DisplayGeoTIFFDetailsOnCLI();
 	DisplayBitmapOnCLI();
 	UnloadGeoTIFF();
 	std::cout << "FINISHED!" << std::endl; //test
